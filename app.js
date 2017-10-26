aedc4785364f8a9b8d264047412134b7036d1b48
Benwager12 <benwager@live.co.uk>
2017-10-26 16:59:56 +0100

    Added admin site
    Added admin site which is the /admin

--- app.js
+++ app.js
@@ -9,20 +9,38 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html')
 });
 
+app.get('/admin', function(req, res) {
+    res.sendFile(__dirname + '/admin.html')
+});
+
 io.sockets.on('connection', function(socket) {
     socket.on('send message', function(data) {
         io.sockets.emit('new message', data);
+        console.log(data);
     });
 
     socket.on('person joined', function(data) {
         io.sockets.emit('person joined', data);
+        console.log(data);
     });
 
     socket.on('name change', function(data) {
         io.sockets.emit('name change', data);
+        console.log(data);
     });
 
     socket.on('send image', function(data) {
         io.sockets.emit('send image', data);
+        console.log(data);
     })
+
+    socket.on('admin message', function(data) {
+        io.sockets.emit('admin message', data);
+        console.log("Admin message: " + data);
+    });
+
+    socket.on('set messages', function(data) {
+        io.sockets.emit('set messages', data);
+        console.log("Messages set to: " + data);
+    });
 });
